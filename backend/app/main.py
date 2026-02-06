06176c7b2dd7ce1cd757de3f9423f57d567b2360
Nuwantha97 <nuwanthasiriwardhana@gmail.com>
2026-02-07 03:51:16 +0530

    feat(backend,frontend): add new project structure

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/home")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
